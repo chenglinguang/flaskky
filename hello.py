6379c18828f7e033e46a9f4b01df9eb32b87eb17
chenglinguang <clg19881220@163.com>
2016-07-02 17:22:06 +0800

    flasky migration

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
+#!/usr/bin/env python3 
+
+#-*-coding:utf-8-*-
+
 import os
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from flask.ext.wtf import Form
+#from flask.ext.script import Manager
+#from flask.ext.bootstrap import Bootstrap
+#from flask.ext.moment import Moment
+#from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flasl.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.script import Shell
+#from flask.ext.migrate import Migrate,MigrateCommand
+from flask_script import Manager,Shell
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate,MigrateCommand
 
 
 basedir=os.path.abspath(os.path.dirname(__file__))
@@ -16,15 +28,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db=SQLAlchemy(app)
+migrate=Migrate(app,db)
 
 class Role(db.Model):
     __tablename__='roles'
-    id=db.Column(db.integer,primary_key=True)
+    id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(64),unique=True)
     users=db.relationship('User',backref='role',lazy='dynamic')
 
@@ -45,6 +59,10 @@ class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=make_shell_context))
+manager.add_command('db',MigrateCommand)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -60,16 +78,19 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name=session.get('name')
-        if old_name is not None and old_name!=form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
+        user=User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user=User(username=form.name.data)
+            db.session.add(user)
+            session['known']=False
+        else:
+            session['known']=True
+        session['name']=form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),known=session.get('known',False))
 
 
 if __name__ == '__main__':
-    db.create_all()
     manager.run()
 
 
